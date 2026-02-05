9babc6c00255719d5d01ad7c4c91412efe3dfadc
karolinamp <karolina.m@paloma.co.uk>
2026-02-05 08:32:03 +0000

    add email key

--- api/send-email.js
+++ api/send-email.js
@@ -1,4 +1,6 @@
 export default async function handler(req, res) {
+    console.log('API Key exists:', !!process.env.BREVO_API_KEY)
+    console.log('API Key starts with:', process.env.BREVO_API_KEY?.substring(0, 10))
     if (req.method !== 'POST') {
       return res.status(405).json({ error: 'Method not allowed' })
     }
